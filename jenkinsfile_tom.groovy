3537a63e6b7de7c28c908302338abcc998f3a2d3
99yaswanth <yashwanth24k@gmail.com>
2021-11-18 22:07:18 +0530

    updated

--- jenkinsfile_tom.groovy
+++ jenkinsfile_tom.groovy
@@ -5,17 +5,13 @@ pipeline{
         stage("tomcat installation"){
             steps{
                 println "install tomcat"
-                sh """
-                root ALL=(ALL) NOPASSWD:ALL
-                jenkins ALL=(ALL) NOPASSWD:ALL
-                
-                ssh -i /tmp/nvirginia.pem ec2-user@44.199.232.231
-                 sudo yum install tomcat -y
-                 sudo yum install tomcat-webapps tomcat-admin-webapps tomcat-docs-webapp tomcat-javadoc -y
-                 sudo systemctl start tomcat
+               sh "ssh -i /tmp/nvirginia.pem ec2-user@44.199.232.231"
+               sh  "sudo yum install tomcat -y"
+                sh "sudo yum install tomcat-webapps tomcat-admin-webapps tomcat-docs-webapp tomcat-javadoc -y"
+                sh "sudo systemctl start tomcat"
 
-                chkconfig tomcat on
-                """
+               sh "chkconfig tomcat on"
+                
 
             }
         }
