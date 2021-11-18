77658af5294b2bc478a424534d2dded100756e8d
99yaswanth <yashwanth24k@gmail.com>
2021-11-18 21:33:27 +0530

    added sudo

--- jenkinsfile_tom.groovy
+++ jenkinsfile_tom.groovy
@@ -7,9 +7,9 @@ pipeline{
                 println "install tomcat"
                 sh """
                 ssh -i /tmp/nvirginia.pem ec2-user@44.199.232.231
-                 yum install tomcat -y
-                 yum install tomcat-webapps tomcat-admin-webapps tomcat-docs-webapp tomcat-javadoc -y
-                 systemctl start tomcat
+                 sudo yum install tomcat -y
+                 sudo yum install tomcat-webapps tomcat-admin-webapps tomcat-docs-webapp tomcat-javadoc -y
+                 sudo systemctl start tomcat
 
                 chkconfig tomcat on
                 """
